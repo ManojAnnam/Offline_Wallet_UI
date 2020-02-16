f5e1b6db10618e4684815ab44444d028cdf3433a
Pavan Kosanam <pavan.kosanam@ggktech.com>
2020-02-16 16:39:34 +0530

    changes

--- offline-wallet-UI/src/app/app-routing.module.ts
+++ offline-wallet-UI/src/app/app-routing.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { LoginComponent, RegisterComponent, HomeComponent,QrCodeGeneratorComponent } from './components';
+import { LoginComponent, RegisterComponent, HomeComponent, QrCodeGeneratorComponent, PaymentComponent } from './components';
 
 const routes: Routes = [
 
   { path: '', component: HomeComponent }, // , canActivate: [AuthGuard]
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
-  { path: 'QrCodeGenerator', component: QrCodeGeneratorComponent },
+  { path: 'payment', component: PaymentComponent },
+  { path: 'new-payment', component: QrCodeGeneratorComponent },
 
   // otherwise redirect to home
   { path: '**', redirectTo: '' }
