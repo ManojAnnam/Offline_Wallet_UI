4f81bacd327e3830591a0648e96d04f6b3dc4aac
Venkataramana Govindu <Venkataramana.Govindu@ggktech.com>
2020-02-16 14:55:20 +0530

    Added qr code generator

--- offline-wallet-UI/src/app/app.module.ts
+++ offline-wallet-UI/src/app/app.module.ts
@@ -3,16 +3,25 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { QrCodeGeneratorComponent } from './qr-code-generator/qr-code-generator.component';
+import {FormsModule} from '@angular/forms';
+import { NgxQRCodeModule } from 'ngx-qrcode2';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+   declarations: [
+      AppComponent,
+      QrCodeGeneratorComponent
+   ],
+   imports: [
+      BrowserModule,
+      AppRoutingModule,
+      FormsModule,
+      NgxQRCodeModule
+
+   ],
+   providers: [],
+   bootstrap: [
+      AppComponent
+   ]
 })
 export class AppModule { }
---
+++ offline-wallet-UI/src/app/qr-code-generator/qr-code-generator.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-qr-code-generator',
+  templateUrl: './qr-code-generator.component.html',
+  styleUrls: ['./qr-code-generator.component.css']
+})
+export class QrCodeGeneratorComponent  {
+  // tslint:disable-next-line: ban-types
+  WalletAmount: Number = 100;
+  // tslint:disable-next-line: ban-types
+  AmounToBePaid: Number;
+  title = 'generate-qrcode';
+  elementType: 'url' | 'canvas' | 'img' = 'url';
+  value: string;
+  display = false;
+  href: string;
+  generateQRCode() {
+    if (!this.AmounToBePaid) {
+      this.display = false;
+      alert('Please enter the name');
+      return;
+    } else if (this.AmounToBePaid > this.WalletAmount) {
+      alert('Sorry! you don\'t have enough balance');
+      return;
+    } else {
+      // tslint:disable-next-line: prefer-const
+      var UserDetails = JSON.parse(localStorage.getItem('UserDetails'));
+      UserDetails.AmountToBePaid = this.AmounToBePaid;
+      UserDetails.Transactionid = 101012010201;
+      this.value = JSON.stringify(UserDetails)
+      this.display = true;
+    }
+  }
+  downloadImage() {
+    this.href = document.getElementsByTagName('img')[0].src;
+  }
+
+}
